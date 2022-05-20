f050cea334363f220011281e5c3f2b97dc885a29
Garnik Ghazaryan <garnik_ghazaryan@epam.com>
2022-05-20 22:16:30 +0400

    and these

---
+++ Sources/BaseClasses/Context/Services.swift
@@ -0,0 +1,11 @@
+//
+//  Services.swift
+//  Velvioo
+//
+//  Created by Garnik Ghazaryan on 2/29/20.
+//  Copyright © 2020 Garnik Ghazaryan. All rights reserved.
+//
+
+import Foundation
+
+class Services {}
