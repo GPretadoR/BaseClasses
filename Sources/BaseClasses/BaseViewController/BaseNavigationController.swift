14489d427fdcb1ac252f7c553bce508eea490262
Garnik Ghazaryan <garnik_ghazaryan@epam.com>
2024-04-14 01:56:28 +0400

    Manoa

--- Sources/BaseClasses/BaseViewController/BaseNavigationController.swift
+++ Sources/BaseClasses/BaseViewController/BaseNavigationController.swift
@@ -17,7 +17,7 @@ open class BaseNavigationController: UINavigationController {
     // Very bad workaround
     weak var coordinator: BaseCoordinator?
 
-    var isPoppingViewController: Bool = false
+    private var isPoppingViewController: Bool = false
 
     public override init(rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController)
@@ -37,20 +37,10 @@ open class BaseNavigationController: UINavigationController {
     @discardableResult
     public override func popViewController(animated: Bool) -> UIViewController? {
         let poppedVC = super.popViewController(animated: animated)
-
         isPoppingViewController = true
-        print("Swipe POPPED", poppedVC)
-//        if let coordinatorVC = coordinator?.controller,
-//           poppedVC == coordinatorVC,
-//           var coordinator,
-//           let parent = coordinator.parentCoordinator {
-//
-//            parent.removeChildCoordinator(coordinator)
-//            coordinator = parent
-//        }
         return poppedVC
     }
-    
+
     @discardableResult
     public override func popToRootViewController(animated: Bool) -> [UIViewController]? {
         let poppedVCs = super.popToRootViewController(animated: animated)
@@ -62,10 +52,6 @@ open class BaseNavigationController: UINavigationController {
 }
 
 extension BaseNavigationController: UINavigationControllerDelegate {
-    public func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-        print("Swipe Will Show", viewController)
-    }
-
     public func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
         if isPoppingViewController {
             print("Swipe Did Show", viewController)
@@ -74,19 +60,10 @@ extension BaseNavigationController: UINavigationControllerDelegate {
             coordinator = parent
         }
     }
-
-    public func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        print("Swipe From VC", fromVC)
-        print("Swipe TO VC", toVC)
-        return nil
-    }
 }
 
 extension BaseNavigationController: UIGestureRecognizerDelegate {
     public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-//        let parent = coordinator?.parentCoordinator
-//        parent?.removeAllChildCoordinators()
-//        coordinator = parent
-        return true
+        return viewControllers.count > 1
     }
 }
